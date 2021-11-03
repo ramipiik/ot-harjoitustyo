8e022fe21fea8d0daecbc32fb50efc77f5921bfb
Rami Piik <rami.piik@gmail.com>
2021-11-04 00:30:25 +0200

    add unit tests for maksupaate.py

--- laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -15,12 +15,22 @@ class TestKassapaate(unittest.TestCase):
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
         self.assertEqual(vaihtoraha, 60)
         self.assertEqual(self.kassapaate.edulliset,1)
+        
+        vaihtoraha=self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100640)
+        self.assertEqual(vaihtoraha, 100)
+        self.assertEqual(self.kassapaate.maukkaat,1)
     
     def test_kateisosto_kun_raha_ei_riita(self):
         vaihtoraha=self.kassapaate.syo_edullisesti_kateisella(200)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
         self.assertEqual(vaihtoraha, 200)
         self.assertEqual(self.kassapaate.edulliset,0)
+
+        vaihtoraha=self.kassapaate.syo_maukkaasti_kateisella(200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        self.assertEqual(vaihtoraha, 200)
+        self.assertEqual(self.kassapaate.maukkaat,0)
 
     def test_korttiosto_kun_raha_riittaa(self):
         self.assertTrue(self.kassapaate.syo_edullisesti_kortilla(self.maksukortti))
@@ -43,5 +53,9 @@ class TestKassapaate(unittest.TestCase):
     
     def test_lataa_rahaa_kortille(self):
         self.kassapaate.lataa_rahaa_kortille(self.maksukortti,1)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100001)
+        self.assertEqual(self.maksukortti.saldo, 1001)
+
+        self.kassapaate.lataa_rahaa_kortille(self.maksukortti,-1)
         self.assertEqual(self.kassapaate.kassassa_rahaa, 100001)
         self.assertEqual(self.maksukortti.saldo, 1001)
